2907907effd9aa287ba7b986c3579863415d3dae
jihwang <b3hwang@ucsd.edu>
2017-03-10 12:35:31 -0800

    added change password/change email

--- public/wireframes-vanilla/js/profile.js
+++ public/wireframes-vanilla/js/profile.js
@@ -81,18 +81,85 @@ function deleteBio() {
   .catch(function(error) {
     console.log("Remove failed: " + error.message);
   });
-
 }
-
 
 function displayBio(text) {
   console.log(text);
   $("#bioText").text(text);
 }
 
-function change
-const user = firebase.auth().currentUser;
-const credential = firebase.auth.EmailAuthProvider.credential(
-    user.email,
-    userProvidedPassword
-);
+function getCredentials(password) {
+  const user = firebase.auth().currentUser;
+  return credential = firebase.auth.EmailAuthProvider.credential(
+      user.email,
+      password
+  );
+}
+
+function validatePassword(password, password2) {
+  if(password.length < 5) {
+    $("#registerPassword").animate({border: '2px red'}, "fast");
+    return "Password must be greater than 5 characters\n";
+  }
+  if(password != password2) {
+    $("#registerPassword2").animate({border: '2px red'}, "fast");
+    return "Passwords do not match\n";
+  }
+  return "";
+}
+
+function validateEmail(email) {
+  var re = /\S+@\S+/
+  //re.test(email)
+  if(true) {
+    return "";
+  } else {
+    return "Enter a valid email";
+  }
+}
+
+function updateInfo() {
+  const password = $("#newPassword").val();
+  const password2 = $("#newPassword2").val();
+  const email = $("#newEmail").val();
+  const currentPassword = $("#currentPassword").val();
+
+  var error = validatePassword(currentPassword, currentPassword);
+  if(error != "") {
+    alert(error);
+    return;
+  }
+
+  const credential = getCredentials(currentPassword);
+
+  if(password.length > 0) {
+    error = validatePassword(password, password2);
+  }
+  if(email.length > 0) {
+    error += validateEmail(email);
+  }
+  if(error != "") {
+      alert(error);
+      return;
+  }
+
+  firebase.auth().currentUser.reauthenticate(credential)
+    .then(function() { console.log(password);
+      if(password.length > 0) {
+        firebase.auth().currentUser.updatePassword(password).then(function() {
+          firebase.auth().signInWithEmailAndPassword(firebase.auth().currentUser.email, password).catch(function(error) {
+            console.log(error);
+            alert(error);
+          });
+          alert("Changed Password!");
+        }).catch(function(error) {
+          console.log(error);
+          alert(error);
+        });
+      }
+    }).then(function() { if(email.length > 0) { firebase.auth().currentUser.updateEmail(email); alert("Changed Email!"); } })
+    .catch(function(error) {
+      console.log(error);
+      alert(error);
+  });
+}
