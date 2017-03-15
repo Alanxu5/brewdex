08be56eca1d8e77bbfe029732c59f8cdd5d0a2e4
Alan Xu <ayx002@ucsd.edu>
2017-03-14 23:45:58 -0700

    stil lcant figure out dropdown

--- public/wireframes-vanilla/js/review.js
+++ public/wireframes-vanilla/js/review.js
@@ -31,7 +31,61 @@ function updateReview() {
   ref1.update({
     "review": updatedReview
   });
-  location.reload();
+  // location.reload();
+}
+
+function displayReview() {
+  $("#ReviewArea").load("#ReviewArea");
+  console.log("displayreview()");
+  // var t = document.querySelector('#reviewRow'),
+  // td = t.content.querySelectorAll("td");
+  //
+  // var ref = firebase.database().ref('BallastPoint');
+  // ref.on('value', function(snapshot) {
+  //   var reviewArr = [];
+  //   var userArr = [];
+  //   var data = snapshot.val();
+  //   for (var key in data) {
+  //     if (data.hasOwnProperty(key)) {
+  //       reviewArr.push(data[key].review);
+  //       userArr.push(data[key].uid);
+  //     };
+  //   }
+  //   var i;
+  //   for(i = 0; i < reviewArr.length; i++){
+  //     td[0].textContent = userArr[i];
+  //     td[1].textContent = reviewArr[i];
+  //     var tb = document.querySelector("tbody");
+  //     var clone = document.importNode(t.content, true);
+  //     tb.appendChild(clone);
+  //   }
+  // });
+  // // <!--Delete/Update Reviews-->
+  // var template = document.querySelector('#deleteRow'); // obj ref to template
+  // var currUid = firebase.auth().currentUser; // need the specific user to display their reviews
+  // if (currUid) {
+  //   currUid = currUid.uid;
+  // }
+  // var ref = firebase.database().ref('BallastPoint');
+  // ref.on('value', function(snapshot) {
+  //   var reviewArr = [];
+  //   var reviewIDArr = [];
+  //   var data = snapshot.val();
+  //   for (var key in data) {
+  //     if (data.hasOwnProperty(key) && data[key].uid == currUid) {
+  //       reviewArr.push(data[key].review);
+  //       reviewIDArr.push(key);
+  //     };
+  //   }
+  //   var i;
+  //   for(i = 0; i < reviewArr.length; i++){
+  //     var clone = template.content.cloneNode(true);
+  //     var option = clone.querySelectorAll('option');
+  //     option[0].id += reviewIDArr[i];
+  //     option[0].innerHTML = reviewArr[i];
+  //     template.parentNode.appendChild(clone);
+  //   }
+  // });
 }
 
 function deleteReview() {
@@ -41,5 +95,4 @@ function deleteReview() {
   var ref1 =  firebase.database().ref("user-reviews/").child(uid).child(reviewID);
   ref.remove();
   ref1.remove();
-  location.reload();
 }
