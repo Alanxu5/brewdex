48cf031073576c68ad350c6a97d23b3801ff25aa
Alan Xu <ayx002@ucsd.edu>
2017-03-16 17:26:06 -0700

    most stuff is working

--- public/wireframes-vanilla/js/review.js
+++ public/wireframes-vanilla/js/review.js
@@ -1,98 +1,127 @@
 function writeReview() {
-  var review = $("#review").val();
-  url = window.location.href;
-  currentLocation = url.split('/').pop();
-  currentLocation = currentLocation.split('.')[0];
-  var location;
-  uid = firebase.auth().currentUser.uid;
-
-  var reviewData = {
-    uid: uid,
-    review: review,
-    location: currentLocation
-  };
-  var newReviewKey = firebase.database().ref("reviews/").child('reviews').push().key;
-  var updates = {};
-  updates['/' + currentLocation + '/' + newReviewKey] = reviewData;
-  updates['/user-reviews/' + uid + '/' + newReviewKey] = reviewData;
-  return firebase.database().ref().update(updates);
+  firebase.auth().onAuthStateChanged(function(user) {
+    if(user){
+      var uid = user.uid; // need the specific user to display their reviews
+      var displayName = user.displayName;
+      console.log("Beginning of writeReview()");
+      var review = $("#review").val();
+      url = window.location.href;
+      currentLocation = url.split('/').pop();
+      currLocation = currentLocation.split('.')[0];
+      var location;
+      var reviewData = {
+        uid: uid,
+        review: review,
+        location: currentLocation
+      };
+      var newReviewKey = firebase.database().ref("reviews/").child('reviews').push().key;
+      var updates = {};
+      updates['/' + currLocation + '/' + newReviewKey] = reviewData;
+      updates['/user-reviews/' + uid + '/' + newReviewKey] = reviewData;
+      console.log("Value of currLocation in writeReview():" + currLocation);
+      console.log("Value of newReviewKey in writeReview():" + newReviewKey);
+      console.log("Value of userUid in writeReview():" + uid);
+      console.log("Value of reviewData in writeReview():" + reviewData);
+      firebase.database().ref().update(updates);
+      console.log("Finished writing to Firebase - writeReview()");
+      var form = document.getElementById("textForm");
+      form.reset();
+    }
+    else {
+      console.log("User is not verified - writeReview()");
+    }
+  });
 }
 
 function updateReview() {
-  var reviewText = $('#dropdown option:selected').text();
-  var reviewID = $("#dropdown option:selected").attr('id');
-  var uid = firebase.auth().currentUser.uid;
-  var updatedReview = prompt("Would you like to update your review?", reviewText);
-  var ref = firebase.database().ref("BallastPoint/").child(reviewID);
-  ref.update({
-    "review": updatedReview
+  firebase.auth().onAuthStateChanged(function(user) {
+    if(user){
+      var uid = user.uid; // need the specific user to display their reviews
+      var displayName = user.displayName;
+      console.log("Beginning of updateReview()");
+      var reviewText = $('#dropdown option:selected').text();
+      var reviewID = $("#dropdown option:selected").attr('id');
+      var updatedReview = prompt("Would you like to update your review?", reviewText);
+      console.log("Value of the review text - updatedReview()" + updatedReview);
+      var ref = firebase.database().ref("BallastPoint/").child(reviewID);
+      ref.update({
+        "review": updatedReview
+      });
+      var ref1 =  firebase.database().ref("user-reviews/").child(uid).child(reviewID);
+      ref1.update({
+        "review": updatedReview
+      });
+      location.reload();
+    }
+    else {
+      console.log("User is not verified - writeReview()");
+    }
   });
-  var ref1 =  firebase.database().ref("user-reviews/").child(uid).child(reviewID);
-  ref1.update({
-    "review": updatedReview
-  });
-  // location.reload();
 }
 
 function displayReview() {
-  $("#ReviewArea").load("#ReviewArea");
-  console.log("displayreview()");
-  // var t = document.querySelector('#reviewRow'),
-  // td = t.content.querySelectorAll("td");
-  //
-  // var ref = firebase.database().ref('BallastPoint');
-  // ref.on('value', function(snapshot) {
-  //   var reviewArr = [];
-  //   var userArr = [];
-  //   var data = snapshot.val();
-  //   for (var key in data) {
-  //     if (data.hasOwnProperty(key)) {
-  //       reviewArr.push(data[key].review);
-  //       userArr.push(data[key].uid);
-  //     };
-  //   }
-  //   var i;
-  //   for(i = 0; i < reviewArr.length; i++){
-  //     td[0].textContent = userArr[i];
-  //     td[1].textContent = reviewArr[i];
-  //     var tb = document.querySelector("tbody");
-  //     var clone = document.importNode(t.content, true);
-  //     tb.appendChild(clone);
-  //   }
+  firebase.auth().onAuthStateChanged(function(user) {
+    if(user){
+      var displayName = user.displayName;
+      console.log("Beginning of displayReview()");
+      var reviewRef = firebase.database().ref("BallastPoint");
+      reviewRef.limitToLast(1).once('child_added', function(snapshot) {
+        console.log(snapshot.val().review);
+        var t = document.querySelector('#reviewRow'),
+        td = t.content.querySelectorAll("td");
+        console.log(snapshot);
+        td[0].textContent = displayName;
+        td[1].textContent = snapshot.val().review;
+        var tb = document.querySelector("tbody");
+        var clone = document.importNode(t.content, true);
+        tb.appendChild(clone);
+
+        var dropTemplate = document.querySelector('#deleteRow'); // obj ref to template
+        var clone = dropTemplate.content.cloneNode(true);
+        var option = clone.querySelectorAll('option');
+        option[0].id += snapshot.val().uid;
+        option[0].innerHTML =snapshot.val().review;
+        dropTemplate.parentNode.appendChild(clone);
+      });
+    }
+  });
+  // reviewRef.limitToLast(1).once('child_changed', function(snapshot) {
+  //   console.log(snapshot.val().review);
+    // var t = document.querySelector('#reviewRow'),
+    // td = t.content.querySelectorAll("td");
+    // console.log(snapshot);
+    // td[0].textContent = snapshot.val().uid;
+    // td[1].textContent = snapshot.val().review;
+    // var tb = document.querySelector("tbody");
+    // var clone = document.importNode(t.content, true);
+    // tb.appendChild(clone);
+    //
+    // var dropTemplate = document.querySelector('#deleteRow'); // obj ref to template
+    // var clone = dropTemplate.content.cloneNode(true);
+    // var option = clone.querySelectorAll('option');
+    // option[0].id += snapshot.val().uid;
+    // option[0].innerHTML =snapshot.val().review;
+    // dropTemplate.parentNode.appendChild(clone);
   // });
-  // // <!--Delete/Update Reviews-->
-  // var template = document.querySelector('#deleteRow'); // obj ref to template
-  // var currUid = firebase.auth().currentUser; // need the specific user to display their reviews
-  // if (currUid) {
-  //   currUid = currUid.uid;
-  // }
-  // var ref = firebase.database().ref('BallastPoint');
-  // ref.on('value', function(snapshot) {
-  //   var reviewArr = [];
-  //   var reviewIDArr = [];
-  //   var data = snapshot.val();
-  //   for (var key in data) {
-  //     if (data.hasOwnProperty(key) && data[key].uid == currUid) {
-  //       reviewArr.push(data[key].review);
-  //       reviewIDArr.push(key);
-  //     };
-  //   }
-  //   var i;
-  //   for(i = 0; i < reviewArr.length; i++){
-  //     var clone = template.content.cloneNode(true);
-  //     var option = clone.querySelectorAll('option');
-  //     option[0].id += reviewIDArr[i];
-  //     option[0].innerHTML = reviewArr[i];
-  //     template.parentNode.appendChild(clone);
-  //   }
-  // });
+
 }
 
 function deleteReview() {
-  var reviewID = $("#dropdown option:selected").attr('id');
-  uid = firebase.auth().currentUser.uid;
-  var ref = firebase.database().ref("BallastPoint/").child(reviewID);
-  var ref1 =  firebase.database().ref("user-reviews/").child(uid).child(reviewID);
-  ref.remove();
-  ref1.remove();
+  firebase.auth().onAuthStateChanged(function(user) {
+    if(user){
+      var uid = user.uid; // need the specific user to display their reviews
+      var displayName = user.displayName;
+      console.log("Beginning of deleteReview()");
+      var reviewID = $("#dropdown option:selected").attr('id');
+      var ref = firebase.database().ref("BallastPoint/").child(reviewID);
+      var ref1 =  firebase.database().ref("user-reviews/").child(uid).child(reviewID);
+      ref.remove();
+      ref1.remove();
+      console.log("Done - deleteReview()");
+      location.reload();
+    }
+    else {
+      console.log("User is not verified - deleteReview()");
+    }
+  });
 }
